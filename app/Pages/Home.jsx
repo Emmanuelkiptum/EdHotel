095c38d0a443c0ab461c7bdf08f26057cd9c2bc3
Emmanuelkiptum <kiptummeri@gmail.com>
2024-08-12 16:15:23 +0300

    Updated project

--- app/Pages/Home.jsx
+++ app/Pages/Home.jsx
@@ -11,7 +11,7 @@ function Home() {
     <div className=" absolute   text-center text-white w-full  h-full flex items-center justify-between px-20 text-2xl md:text-6xl font-bold">
     <a href="#img3" className="animate__animated animate__slideInLeft" >❮</a> 
     <div className="animate__animated animate__slideInDown">
-    Unveiling the World's <br /> Finest Hotels <br />
+    Unveiling mm the World's <br /> Finest Hotels <br />
     <p className="space-x-2 text-[13px] md:text-[20px] mt-7 flex justify-center">
       <Link href={"/Rooms"} className=" animate__animated animate__slideInLeft px-2 py-1 md:px-4 md:py-4 rounded-md bg-yellow-500">OUR ROOM</Link>
       <Link href={"/Rooms"} className=" animate__animated animate__slideInRight px-2 py-1 md:px-4 md:py-4 rounded-md bg-white text-black">BOOK ROOM</Link>
